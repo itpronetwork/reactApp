91f635fa87802f59c36e02b0331213323f4bf925
Marcin Dobrzaniecki <marcind@yelpcorp.com>
2021-10-23 11:17:37 +0100

    Add gitignore

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 // server/index.js
 const path = require('path');
-const express = require('express');
+const express = require("express");
 
+const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -14,4 +17,8 @@ app.get("/api", (req, res) => {
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
 });
